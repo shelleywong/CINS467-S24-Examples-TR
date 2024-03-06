98b15ad43b1f352f57400de7d263a17a5c90d9b6
Shelley Wong <swong26@csuchico.edu>
2024-03-05 16:39:32 -0800

    adds geolocator example

--- tr_app/lib/main.dart
+++ tr_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 //import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
 
 //import 'storage.dart';
 //import 'sqlstorage.dart';
@@ -59,6 +61,56 @@ class _MyHomePageState extends State<MyHomePage> {
   final CounterStorage _storage = CounterStorage(); // sqflite (SQLite DB storage)
   int _counter = 0;
 
+  late Future<Position> _position;
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+  late StreamSubscription<Position> positionStream;
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  /// Ref: https://pub.dev/packages/geolocator
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the 
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately. 
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   Future<void> _incrementCounter() async {
     // final SharedPreferences prefs = await _prefs;
     // final int counter = (prefs.getInt('counter') ?? 0) + 1;
@@ -98,12 +150,22 @@ class _MyHomePageState extends State<MyHomePage> {
         _counter = value;
       });
     });
+    _position = _determinePosition();
+    positionStream = Geolocator.getPositionStream(
+      locationSettings: locationSettings).listen((Position? pos){
+        // Handle position changes
+        if(kDebugMode){
+          print(pos == null ? 'Position Unknown' : '${pos.latitude.toString()}, ${pos.longitude.toString()}');
+        }
+      }
+    );
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     //_storage.close();  // sqflite example
+    positionStream.cancel();
     super.dispose();
   }
 
@@ -144,6 +206,21 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            FutureBuilder(
+              future: _position,
+              builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.waiting:
+                    return const CircularProgressIndicator();
+                  default:
+                    if(snapshot.hasError){
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return Text('Location: ${snapshot.data}, Accuracy: ${snapshot.data!.accuracy}');
+                    }
+                }
+              },
+            ),
             Container(
               alignment: Alignment.center,
               constraints: const BoxConstraints(
